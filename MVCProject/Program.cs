fc9037d95117f7169b8fe0722dd18ecc012db081
DoganBozkurt <doqan228@gmail.com>
2023-12-23 00:43:40 +0300

    tamamen çalışan hali admin tarafı hariç

--- MVCProject/Program.cs
+++ MVCProject/Program.cs
@@ -6,10 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//DI
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
